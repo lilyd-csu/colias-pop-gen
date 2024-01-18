1c989f97f388209fe93c9d4c4749b5ff3fcc4c82
Lily Durkee <l.durkee@colostate.edu>
2024-01-18 12:26:48 -0700

    first commit

---
+++ Colias_map.R
@@ -0,0 +1,186 @@
+# first attempt at map
+
+setwd("/Users/lilydurkee/OneDrive - Colostate/Grad School/R-Projects-Grad/Colias")
+
+
+library(ggplot2)
+library(tidyr)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggspatial)
+library(googleway)
+library(ggrepel)
+library(usmap)
+library(maps)
+
+######## maps #########
+state <- map_data("state")
+#co_wy1 <- subset(state, region=="colorado" | region=="wyoming")
+counties <- map_data("county")
+#co_wy2 <- subset(counties, region=="colorado" | region=="wyoming")
+
+sites <- read.csv("sites-all1.csv")
+sites$pair <- ifelse(sites$name=="SC", "E2", ifelse(
+                     sites$name=="WY", "WY", sites$pair))
+
+CDT <- read.csv("CDT.csv")
+
+#### site pairs only ####
+co_map1 <- ggplot(data=state, mapping=aes(x=long, y=lat, group=group)) + 
+#  geom_raster(data = dem.raster, aes(lon, lat, fill = alt), alpha = .45)+
+#  coord_fixed(1.3) + 
+#  geom_sf(fill="gray90") +  #requires shapefile
+  geom_polygon(color="gray", fill="white") + 
+  #geom_polygon(data=counties, fill=NA, color="lightgray") + 
+  theme_bw()+
+  labs(x="long", y="lat")+
+  #theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+   #    axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  # annotation_north_arrow(location = "bl", which_north = "true", 
+  #                        pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+  #                       style = north_arrow_fancy_orienteering)+
+  geom_point(data=CDT, aes(x=long, y=lat, group=NULL), color="gray60", size=.01, alpha=.1)+
+  geom_point(data=sites, aes(x=x, y=y, group=pair, color=pair, shape=elevation), size=2.7, alpha=.9) +
+  geom_text(hjust=-.3, vjust=.5, data=sites, aes(x=x, y=y, label=name, group=elevation), size=3) +
+  scale_color_manual(values=c("royalblue1", "royalblue2", "royalblue3", "royalblue4", "salmon 1", "salmon 2", "salmon3", "steelblue"))+
+  coord_sf(xlim = c(-110, -102), ylim = c(37, 42))+
+  annotate("text", x=-103.5, y=37.5, size=5, label= "Colorado")+
+  scale_shape_manual(values=c(17, 15))
+
+co_map1
+
+#### sites grouped by elevation ####
+
+co_map2 <- ggplot(data=state, mapping=aes(x=long, y=lat, group=group)) + 
+  #  geom_raster(data = dem.raster, aes(lon, lat, fill = alt), alpha = .45)+
+  #  coord_fixed(1.3) + 
+  #  geom_sf(fill="gray90") +  #requires shapefile
+  geom_polygon(color="gray", fill="white") + 
+  #geom_polygon(data=counties, fill=NA, color="lightgray") + 
+  theme_bw()+
+  labs(x="long", y="lat")+
+  #theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+  #    axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  #annotation_north_arrow(location = "bl", which_north = "true", 
+  #    pad_x = unit(0.75, "in"), pad_y = unit(0.5, "in"),
+  #  style = north_arrow_fancy_orienteering)
+  geom_point(data=CDT, aes(x=long, y=lat, group=NULL), color="gray60", size=.01, alpha=.1)+
+  geom_point(data=sites, aes(x=x, y=y, group=elevation, color=elevation), shape=17, size=2.7, alpha=.9) +
+  geom_text(hjust=-.3, vjust=.5, data=sites, aes(x=x, y=y, label=name, group=elevation), size=3) +
+  scale_color_manual(values=c("dodgerblue", "goldenrod"))+
+  coord_sf(xlim = c(-110, -102), ylim = c(37, 42))+
+  annotate("text", x=-103.5, y=37.5, size=5, label= "Colorado")
+
+co_map2
+
+
+ #### all individuals ####
+
+waypts <- read.csv("waypoints-all.csv")
+
+# all individuals
+co_map3 <- ggplot(data=waypts, mapping=aes(x=long, y=lat, group=group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color="black", fill="gray80") + 
+  geom_polygon(data=co_county, fill=NA, color="white") + 
+  geom_polygon(color="gray60", fill=NA) + 
+  theme_minimal()+
+  labs(x="long", y="lat")+
+  geom_point(data=CDT, aes(x=long, y=lat, group=NULL), color="indianred", size=.00001)+
+  geom_point(data=waypts, aes(x=x, y=y, group=elevation, color=elevation), shape=17, size=2) +
+  geom_text(hjust=-.5, vjust=.5, data=waypts, aes(x=x, y=y, label=name, group=elevation), size=1.5) +
+  scale_color_manual(values=c("dodgerblue", "goldenrod"))+
+  ylim(36.8, 41.2)+
+  xlim(-109.2, -101.4)#+
+  #annotate("text", x=-106.2, y=37.8, size=2.5, label= "Continental Divide")
+
+co_map3
+
+
+
+######## expected results figures #########
+sites.fig <- read.csv("sites.csv")
+
+exp1 <- ggplot(data=sites.fig, mapping=aes(x=exp.x1, y=exp.y1, group=elevation)) + 
+  geom_point(aes(color=pair), shape=17, size=4) +
+  geom_text(hjust=-.5, vjust=.5, data=sites.fig, aes(label=name), size=3) +
+  #scale_color_manual(values=c("dodgerblue", "goldenrod"))+
+  theme_minimal() +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+         axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  ylim(0,70)+
+  xlim(0,70)+
+  labs(x="PC1", y="PC2")
+
+exp1 
+
+sites.fig <- read.csv("sites.csv")
+
+exp2 <- ggplot(data=sites.fig, mapping=aes(x=exp.x2, y=exp.y2, group=elevation)) + 
+  geom_point(aes(color=elevation), shape=17, size=4) +
+  geom_text(hjust=-.5, vjust=.5, data=sites.fig, aes(label=name), size=3) +
+  scale_color_manual(values=c("dodgerblue", "goldenrod"))+
+  theme_minimal()+
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  xlim(0, 30)+
+  ylim(20, 60)+
+  labs(x="PC1", y="PC2")
+
+exp2
+
+exp3 <- ggplot(data=sites.fig, mapping=aes(x=exp.x3, y=exp.y3, group=include)) + 
+  geom_point(aes(color=include), shape=17, size=4) +
+  geom_text(hjust=-.5, vjust=.5, data=sites.fig, aes(label=name), size=3) +
+  scale_color_manual(values="gray")+
+  theme_minimal()+
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  xlim(0, 100)+
+  ylim(0, 100)+
+  labs(x="PC1", y="PC2")
+
+exp3
+
+IBD <- read.csv("IBD.csv")
+exp4 <- ggplot(data=IBD, mapping=aes(x=dist, y=Fst)) + 
+  geom_point(shape=21, size=3, fill="red") +
+  geom_smooth(se=FALSE, color="black", method="lm")+
+ # scale_fill_manual(values=c("red", "orange", "yellow"))+
+  theme_minimal()+
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(),
+        axis.text.y=element_blank(), axis.ticks.y=element_blank())+
+  labs(x="elevation or distance", y="genetic distance")
+
+exp4
+
+ggarrange(exp1, exp2, exp4, nrow=3)
+
+
+######### Pollard walk figure #########
+
+setwd("/Users/lilydurkee/OneDrive - Colostate/Grad School/R-Projects-Grad/Colias")
+field_data1 <- read.csv("Colias-field-data-summer22.csv")
+field_data <- field_data1 %>% filter(scudderi == 0 & alexandra == 0) %>%
+  group_by(site_ID, elevation) %>%
+  summarize(colias_num = mean(colias_num))
+
+field1 <- lm(colias_num ~ elevation, data=field_data)
+summary(field1)
+
+model1_data <- data.frame(summary(emmeans(field1, ~ elevation)))
+emmeans(field1, pairwise ~ elevation)
+
+ggplot() + 
+  geom_jitter(data=field_data, mapping=aes(x=elevation, y=colias_num, color=elevation), 
+              width=.1, size=3, alpha=.5) +
+  geom_pointrange(data=model1_data, aes(x=elevation, y=emmean, ymin=lower.CL, ymax=upper.CL, color=elevation),
+                  size=1)+
+  labs(x="elevation", y="Colias count")+
+  theme_classic()+
+  #scale_fill_manual(values=c("blue", "yellow"))+
+  scale_color_manual(values=c("dodgerblue", "goldenrod"))
+
+
+
---
+++ pcangsd.R
@@ -0,0 +1,186 @@
+# pcangsd
+
+setwd("/Users/lilydurkee/OneDrive - Colostate/Grad School/R-Projects-Grad/Colias")
+
+#### 4.0x ####
+
+#upload covariance matrix
+cov<-as.matrix(read.table("colias-full-down4.0x.cov"))
+
+#eigenvalues
+e<-eigen(cov)
+
+#make dataframe
+e.df <- as.data.frame(e$vectors)
+e.df$ID <- names$V1
+e.df$name <- substr(e.df$ID, 1, 2)
+
+e.df <- e.df %>% dplyr::select(c(V1, V2, ID, name))
+
+#upload all info about sites
+sites <- read.csv("sites-all1.csv")
+
+pca_4.0x.data <- merge(e.df, sites, by="name", all=T)
+pca_4.0x.data$cov <- "4.0x"
+
+
+#simple PCA
+plot(e$vectors[,1:2], xlim=c(-.1, .1))
+
+#with ggplot
+library(ggplot2)
+plot4.0x <- ggplot(data=pca_4.0x.data, aes(x = V1, y = V2, 
+                           #color=name, 
+                           color=elevation)) +
+  geom_point()+
+  labs(x="PC1", y="PC2") +
+  xlim(-.125, .025) +
+  ylim(-.1, .35) +
+  geom_text(aes(label=ID), color="black", size=2.5, vjust=.5, hjust=.5)
+
+plot4.0x
+
+## Notes: FV14 clumps with C. euretheme individuals
+## use: filter(pca_4.0x.data, name != "Eu")
+
+#amount that can be explained by each variable
+e$values/sum(e$values)
+
+#read in full bamlist, IDs only
+names<-read.table("colias-full-down4.0x-bam-list.ind")
+
+
+#### 3.5x ####
+
+#upload covariance matrix
+cov<-as.matrix(read.table("colias-full-down3.5x.cov"))
+
+#eigenvalues
+e<-eigen(cov)
+
+#make dataframe
+e.df <- as.data.frame(e$vectors)
+e.df$ID <- names$V1
+e.df$name <- substr(e.df$ID, 1, 2)
+
+e.df <- e.df %>% dplyr::select(c(V1, V2, ID, name))
+
+#upload all info about sites
+sites <- read.csv("sites-all1.csv")
+
+pca_3.5x.data <- merge(e.df, sites, by="name", all=T)
+pca_3.5x.data$cov <- "3.5x"
+
+#simple PCA
+plot(e$vectors[,1:2], xlim=c(-.1, .1))
+
+#with ggplot
+plot3.5x <- ggplot(data=pca_3.5x.data, aes(x = V1, y = V2, 
+                           #color=name, 
+                           color=elevation)) +
+  geom_point()+
+  labs(x="PC1", y="PC2") +
+  xlim(-.125, .025) +
+  ylim(-.1, .35) +
+  geom_text(aes(label=ID), color="black", size=2.5, vjust=.5, hjust=.5)
+
+plot3.5x
+
+#### 3.0x ####
+
+#upload covariance matrix
+cov<-as.matrix(read.table("colias-full-down3.0x.cov"))
+
+#eigenvalues
+e<-eigen(cov)
+
+#make dataframe
+e.df <- as.data.frame(e$vectors)
+e.df$ID <- names$V1
+e.df$name <- substr(e.df$ID, 1, 2)
+
+e.df <- e.df %>% dplyr::select(c(V1, V2, ID, name))
+
+#upload all info about sites
+sites <- read.csv("sites-all1.csv")
+
+pca_3.0x.data <- merge(e.df, sites, by="name", all=T)
+pca_3.0x.data$cov <- "3.0x"
+
+#simple PCA
+plot(e$vectors[,1:2], xlim=c(-.1, .1))
+
+#with ggplot
+plot3.0x <- ggplot(data=pca_3.5x.data, aes(x = V1, y = V2, 
+                                           #shape=elevation, 
+                                           color=elevation)) +
+  geom_point(size=2)+
+  labs(x="PC1", y="PC2") +
+  xlim(-.125, .025) +
+  ylim(-.1, .35) +
+  geom_text(aes(label=ID), color="black", size=2.5, vjust=.5, hjust=.5)
+
+plot3.0x
+
+
+### all ###
+pca_all <- rbind(pca_3.0x.data, pca_3.5x.data, pca_4.0x.data)
+
+pca_all$category1 <- ifelse(pca_all$name=="Eu", "C. eurytheme", 
+                           ifelse(pca_all$name=="AL", "C. alexandra", pca_all$elevation))
+
+pca_all$category2 <- ifelse(pca_all$name=="Eu", "C. eurytheme", 
+                            ifelse(pca_all$name=="AL", "C. alexandra", 
+                                   ifelse(pca_all$name=="WY", "WY", 
+                                          ifelse(pca_all$name=="SC", "E2", pca_all$pair))))
+
+
+
+pca_all$ID2 <- ifelse(pca_all$ID == "FV15", "FV15", NA)
+
+
+plot_all1 <- ggplot(data=pca_all, aes(x = V1, y = V2, 
+                                           color=category1)) +
+  facet_wrap(~ cov) +
+  geom_point(size=2)+
+  labs(x="PC1", y="PC2") +
+  #xlim(-.125, .025) +
+  #ylim(-.1, .35) +
+  xlim(-.025, .025) +
+  ylim(-.07, .07)+
+  geom_text(aes(label=ID2), color="black", size=2.5, vjust=.5, hjust=.5)
+  
+
+plot_all2 <- ggplot(data=pca_all, aes(x = V1, y = V2, 
+                                      color=category2)) +
+  facet_wrap(~ cov) +
+  geom_point(size=2)+
+  labs(x="PC1", y="PC2") +
+  #xlim(-.125, .025) +
+  #ylim(-.1, .35) +
+  xlim(-.025, .025) +
+  ylim(-.07, .07)+
+  geom_text(aes(label=ID2), color="black", size=2.5, vjust=.5, hjust=.5)
+
+
+plot_all2
+
+#library(ggpubr)
+ggarrange(plot_all1, plot_all2, nrow=2)
+
+plot_all3 <- ggplot(data=filter(pca_all, name != "Eu" & name != "AL"), aes(x = V1, y = V2, 
+                                      color=category2,
+                                      shape=elevation)) +
+  facet_wrap(~ cov) +
+  geom_point(size=2)+
+  labs(x="PC1", y="PC2") +
+  #xlim(-.125, .025) +
+  #ylim(-.1, .35) +
+  xlim(-.025, .025) +
+  ylim(-.07, .07)+
+  geom_text(aes(label=ID2), color="black", size=2.5, vjust=.5, hjust=.5)+
+  scale_color_manual(values=c("red", "orange", "yellow", "green", 
+                              "salmon 1", "salmon 2", "salmon3", "steelblue"))+
+  scale_shape_manual(values=c(17, 15))
+  
+plot_all3
